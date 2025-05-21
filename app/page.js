9adfbfe70bb2a702b4e6d3d645e41c39dff1b62b
Raghav2125 <raghavbirewar@gmail.com>
2025-05-21 15:19:20 +0530

    Header and Navbar

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 import Image from "next/image";
+import Navbar  from "@/components/Navbar/Navbar";
+import Header from "@/components/Header/Header";  
 
 export default function Home() {
   return (
       <main>
-
-        <h1>Main</h1>
-
-
-
-
+        <Navbar />
+        <Header />
       </main>
   );
 }
